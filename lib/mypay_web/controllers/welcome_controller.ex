ed902dd88a470bc6c7ca142d943edda552abc83f
rickutino <rickutino@gmail.com>
2021-02-23 16:08:21 +0900

    connect backend numbers.ex to front

--- lib/mypay_web/controllers/welcome_controller.ex
+++ lib/mypay_web/controllers/welcome_controller.ex
@@ -1,7 +1,23 @@
 defmodule MypayWeb.WelcomeController do
   use MypayWeb, :controller
 
-  def index(connection, _params) do
-    text(connection, "Welcome to the MyPay API")
+  alias Mypay.Numbers
+
+  def index(connection, %{"filename" => filename}) do
+    filename
+    |> Numbers.sum_from_file()
+    |> handle_response(connection)
+  end
+
+  defp handle_response({:ok, %{result: result}}, connection) do
+    connection
+    |> put_status(:ok)
+    |> json(%{message: "Welcome to MyPay API. Here is your number #{result}"})
+  end
+
+  defp handle_response({:error, reason}, connection) do
+    connection
+    |> put_status(:bad_request)
+    |> json(reason)
   end
 end
--- lib/mypay_web/router.ex
+++ lib/mypay_web/router.ex
@@ -8,7 +8,7 @@ defmodule MypayWeb.Router do
   scope "/api", MypayWeb do
     pipe_through :api
 
-    get "/", WelcomeController, :index
+    get "/:filename", WelcomeController, :index
   end
 
   # Enables LiveDashboard only for development
